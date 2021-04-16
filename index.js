9e0c52c03876c32ad43d6ce335798b308c694848
Nathan <nlaundry@uoguelph.ca>
2021-04-16 01:34:36 -0400

    merged nested var extraction and Nausher's recent changes

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function main() {
 
     discord.ws.on("INTERACTION_CREATE", async interaction => {
         const command = interaction.data.name.toLowerCase();
-<<<<<<< HEAD
         const args = interaction.data.options; //
         const uid = interaction.member.user.id;
         const guid = interaction.guild.id
@@ -28,13 +27,6 @@ function main() {
             let member = guild.members.cache.get(uid);
             if (member.roles.cache.has(config.mod_role)) {
                 shuffleCommand(args);
-=======
-        if(command == "shuffle" ){//&& interaction.member.roles.has(config.mod_role)) {
-            let guild = discord.guilds.cache.get(interaction.guild_id);
-            let member = guild.members.cache.get(interaction.member.user.id);
-            if(member.roles.cache.has(config.mod_role)) {
-                shuffleCommand(interaction);
->>>>>>> c036c84cc3b7798237d240b9447936480d66ae77
 
             } else { } //say bad message
 
@@ -76,26 +68,14 @@ function postCommand() {
     discord.api.applications(discord.user.id).guilds(config.server).commands.post(command);
 }
 
-<<<<<<< HEAD
-async function shuffleCommand(args) {
-    let voiceChannel = discord.channels.get(args[0]["value"]); //
-    let members = voiceChannel.members;
-    let channelCount = ~~(members.len / userPerChannel); //rounds to the nearest whole number
-
-
-    for (var i = 0; i < channelCount; i++) {
-        interaction.guild.channels.create("Trivia Night Room #" + (i + 1), { reason: "Trivia Night" })
-            .then(console.log)
-=======
 function shuffleCommand(interaction) {
     let voiceChannel = discord.channels.cache.get(interaction.data.options.value);
     let members = voiceChannel.members;
     let channelCount = members.size / userPerChannel;
 
-    for(var i = 0; i < channelCount; i++) {
-        interaction.guild.channels.create("Trivia Night Room #" + (i + 1), {reason: "Trivia Night"})
+    for (var i = 0; i < channelCount; i++) {
+        interaction.guild.channels.create("Trivia Night Room #" + (i + 1), { reason: "Trivia Night" })
             .then(console.log("hey"))
->>>>>>> c036c84cc3b7798237d240b9447936480d66ae77
             .catch(console.error);
 
         console.log("this works");
